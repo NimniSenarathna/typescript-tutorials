51a43a63ef6552afdb7e9b5275814bb86c9691ec
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-08 01:54:23 +0530

    arrays in typescript

---
+++ Lesson3_Objects_Arrays_Tuples_Enums/build/js/main.js
@@ -0,0 +1,13 @@
+// Arrays
+var stringArr = ['one', 'hey', 'Nimni'];
+var guitars = ['Base', 'Classic', 'Electric', 2023];
+var mixedData = ['EVH', 2003, true];
+stringArr[0] = 'Nimni SE';
+stringArr.push('hey typescript..');
+guitars[0] = 2003;
+guitars.unshift('Bentley');
+// Array anytype
+var emptyArray = [];
+// Array explicit type
+var bands = [];
+bands.push('Van Halen');
---
+++ Lesson3_Objects_Arrays_Tuples_Enums/src/main.ts
@@ -0,0 +1,19 @@
+// Arrays
+let stringArr = ['one', 'hey', 'Nimni']
+
+let guitars = ['Base', 'Classic', 'Electric', 2023]
+
+let mixedData = ['EVH', 2003, true]
+
+stringArr[0] = 'Nimni SE'
+stringArr.push('hey typescript..')
+
+guitars[0] = 2003
+guitars.unshift('Bentley')
+
+// Array anytype
+let emptyArray = []
+
+// Array explicit type
+let bands: string[] = []
+bands.push('Van Halen')
