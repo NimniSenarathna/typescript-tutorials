582290f45701ba7fdb9492cc73dcedfd56a752c3
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-07 22:32:23 +0530

    typescript basic code

---
+++ Lesson1_Start/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let username1 = "Nimni A. Senarathne";
+console.log(username1);
+let a = 20;
+let b = 5;
+let c = 2;
+console.log(a / b);
---
+++ Lesson1_Start/src/main.ts
@@ -0,0 +1,8 @@
+let username1 = "Nimni A. Senarathne"
+console.log(username1)
+
+let a: number = 20
+let b: number = 5
+let c: number = 2
+
+console.log(a / b)
