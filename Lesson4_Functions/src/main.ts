e2f57d12aa6d95fe16a5065d0d0349e19bb7d3ba
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-17 22:10:27 +0530

    type alliases

---
+++ Lesson4_Functions/src/main.ts
@@ -0,0 +1,12 @@
+// Type Alliases
+type stringOrNumber = string | number
+
+type stringOrNumberArray = (string | number)[]
+
+type Guitarist = {
+    name: string,
+    active?: boolean,
+    albums: stringOrNumberArray
+}
+
+type userId = stringOrNumber
