21bac05fc01261c01ed2f16515517858341a91f1
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-22 16:24:00 +0530

    visibility modifiers, private protected examples

--- Lesson6_Classes/build/js/main.js
+++ Lesson6_Classes/build/js/main.js
@@ -1,10 +1,21 @@
 "use strict";
 // Inside a class - properties and methods are referred class member
 class Coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
         this.name = name;
         this.music = music;
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello, I'm age ${this.age}`;
+    }
 }
+const David = new Coder('David', 'Beckham', 20);
+console.log(David.getAge());
+// console.log(David.lang)
+// console.log(David.name)
--- Lesson6_Classes/src/main.ts
+++ Lesson6_Classes/src/main.ts
@@ -1,14 +1,19 @@
 // Inside a class - properties and methods are referred class member
 class Coder {
-    name: string
-    music: string
-    age: number
-    lang: string
+    secondLang!: string
 
-    constructor (name: string, music: string, age: number, lang: string) {
+    constructor (public readonly name: string, public music: string, private age: number, protected lang: string = 'Typescript') {
         this.name = name
         this.music = music
         this.age = age
         this.lang = lang
     }
+    public getAge() {
+        return `Hello, I'm age ${this.age}`;
+    }
 }
+
+const David = new Coder('David', 'Beckham', 20)
+console.log(David.getAge())
+// console.log(David.lang)
+// console.log(David.name)
