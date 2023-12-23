4f5f9b907c3e81898cf4cf6fa073dcf06982011a
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-23 14:54:05 +0530

    static class members and getters setters

--- Lesson6_Classes/build/js/main.js
+++ Lesson6_Classes/build/js/main.js
@@ -42,3 +42,43 @@ class Guitarist {
 }
 const Page = new Guitarist('Jimmy', 'Guitar');
 console.log(Page.play('strums'));
+// -- static class members
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Peeps.count);
+console.log(Steve.id);
+console.log(John.id);
+// -- getters and setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
--- Lesson6_Classes/src/main.ts
+++ Lesson6_Classes/src/main.ts
@@ -58,3 +58,54 @@ class Guitarist implements Musician {
 
 const Page = new Guitarist('Jimmy', 'Guitar')
 console.log(Page.play('strums'))
+
+// -- static class members
+class Peeps {
+    static count: number = 0
+
+    static getCount(): number {
+        return Peeps.count
+    }
+
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        this.id = ++Peeps.count
+    }
+}
+
+const John = new Peeps('John')
+const Steve = new Peeps('Steve')
+const Amy = new Peeps('Amy')
+
+console.log(Peeps.count)
+console.log(Steve.id)
+console.log(John.id)
+
+// -- getters and setters
+
+class Bands {
+    private dataState: string []
+
+    constructor () {
+        this.dataState = []
+    }
+
+    public get data(): string[] {
+        return this.dataState
+    }
+
+    public set data(value: string[]) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value
+            return
+        } else throw new Error ('Param is not an array of strings')
+    }
+}
+
+const MyBands = new Bands()
+MyBands.data = ['Neil Young', 'Led Zep']
+console.log(MyBands.data)
+MyBands.data = [...MyBands.data, 'ZZ Top']
+console.log(MyBands.data)
