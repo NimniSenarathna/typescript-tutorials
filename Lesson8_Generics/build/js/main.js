e8c4e822fe64b523687d574ed792b73f11dfabb3
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-12-27 23:13:40 +0530

    isTrue with keyOf Assertion

--- Lesson8_Generics/build/js/main.js
+++ Lesson8_Generics/build/js/main.js
@@ -9,3 +9,27 @@ console.log(isObj('John'));
 console.log(isObj([1, 2, 3]));
 console.log(isObj({ name: 'John' }));
 console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+console.log('------------\n');
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
